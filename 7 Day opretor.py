d95cc41440926e27bbc1caa80466b29489b1865b
coderguysishere <gauravshegokar2312@gmail.com>
2023-01-09 11:55:09 +0530

    intial commit

---
+++ 7 Day opretor.py
@@ -0,0 +1,12 @@
+print(5+6)
+print(5-6)
+print(5*6)
+print(5/6)
+print(5%3) #remender
+print(5**3) #exponisional
+
+a=int(input("enter first no"))
+b=int(input("enter second no"))
+print("addition is",(a+b))
+print("substraction is ",(a-b))
+print("multiplication is",(a*b))
